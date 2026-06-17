c30097624b877e4b220b4ccf47faa4f87e81706e
André Tachian <altachian@gmail.com>
2026-06-17 12:55:41 -0300

    feat(contracts): implement ST-701 erc721 mint controls

--- smart-contracts/contracts/SuperTrunfoCard.sol
+++ smart-contracts/contracts/SuperTrunfoCard.sol
@@ -7,12 +7,21 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SuperTrunfoCard is ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
+    mapping(address account => bool enabled) private _authorizedMinters;
 
     event CardMinted(address indexed to, uint256 indexed tokenId, string tokenUri);
+    event MinterUpdated(address indexed minter, bool enabled);
+
+    error EmptyTokenUri();
+    error UnauthorizedMinter(address account);
 
     constructor(address initialOwner) ERC721("Super Trunfo Card", "STC") Ownable(initialOwner) {}
 
-    function safeMint(address to, string memory tokenUri) external onlyOwner returns (uint256) {
+    function safeMint(address to, string calldata tokenUri) external onlyAuthorizedMinter returns (uint256) {
+        if (bytes(tokenUri).length == 0) {
+            revert EmptyTokenUri();
+        }
+
         uint256 tokenId = ++_nextTokenId;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, tokenUri);
@@ -20,5 +29,29 @@ contract SuperTrunfoCard is ERC721URIStorage, Ownable {
         emit CardMinted(to, tokenId, tokenUri);
 
         return tokenId;
+    }
+
+    function setMinter(address minter, bool enabled) external onlyOwner {
+        _authorizedMinters[minter] = enabled;
+
+        emit MinterUpdated(minter, enabled);
+    }
+
+    function isMinter(address account) external view returns (bool) {
+        return _authorizedMinters[account];
+    }
+
+    function nextTokenId() external view returns (uint256) {
+        return _nextTokenId + 1;
+    }
+
+    modifier onlyAuthorizedMinter() {
+        address sender = _msgSender();
+
+        if (sender != owner() && !_authorizedMinters[sender]) {
+            revert UnauthorizedMinter(sender);
+        }
+
+        _;
     }
 }
